eca2a7e6a82d75ef8af31d7690ddd8f0f0152288
陈雷昊 <chenleihao@taou.com>
2019-11-18 12:41:00 -0600

    finish share mem method, seems correctness is correct

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -7,7 +7,7 @@
 
 #include <mxnet/base.h>
 #include <stdio.h>
-#include <cublasLt.h>
+//#include "cublasLt.h"
 namespace mxnet
 {
 namespace op
@@ -72,6 +72,59 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
 #undef k4d
 }
 
+
+__global__ void forward_kernel_shared(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K){
+    int n, m, h0, w0, h_base, w_base, h, w;
+    int X_tile_width = TILE_WIDTH + K-1;
+    extern __shared__ float shmem[];
+    float* X_shared = &shmem[0];
+    float* W_shared = &shmem[X_tile_width * X_tile_width];
+
+    const int H_out = H - K + 1;
+    const int W_out = W - K + 1;
+
+    int W_grid = W_out/TILE_WIDTH;
+    if (W_out%TILE_WIDTH != 0)
+        W_grid++;
+#define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
+#define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
+#define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+#define xs2d(i1, i0) X_shared[(i1)*(X_tile_width) + (i0)]
+#define ws2d(i1, i0) W_shared[(i1)* K + (i0)]
+    n = blockIdx.x;
+    m = blockIdx.y;
+    h0 = threadIdx.x;
+    w0 = threadIdx.y;
+    h_base = (blockIdx.z / W_grid) * TILE_WIDTH; // vertical base out data index for the block
+    w_base = (blockIdx.z % W_grid) * TILE_WIDTH; // horizontal base out data index for the block
+    h = h_base + h0;
+    w = w_base + w0;
+    float acc = 0;
+    for(int c = 0; c < C; c++) {
+        if ((h0 < K) && (w0 < K)) {
+            ws2d(h0, w0) = k4d(m, c, h0, w0);
+        }
+        __syncthreads();
+        for (int i = h; i < h_base + X_tile_width; i += TILE_WIDTH) {
+            for (int j = w; j < w_base + X_tile_width; j += TILE_WIDTH) {
+                    xs2d(i - h_base, j - w_base) = (i < H && j < W) ? x4d(n, c, i, j) : 0;
+            }
+        }
+        __syncthreads();
+        for (int p = 0; p < K; p++) {
+            for (int q = 0; q < K; q++) {
+                if((h0 + p) < X_tile_width && (w0 + q) < X_tile_width) acc += xs2d(h0 + p, w0 + q) * ws2d(p, q);
+            }
+        }
+        __syncthreads();
+        if(n < B && m < M && h < H_out && w < W_out) y4d(n, m, h, w) = acc;
+    }
+#undef y4d
+#undef x4d
+#undef k4d
+#undef xs2d
+#undef ws2d
+}
 //__global__ void unroll_Kernel(int C, int H, int W, int K, float* X, float* X_unroll){
 //    int c, s, h_out, w_out, h_unroll, w_base, p, q;
 //    int t = blockId.x * CUDA_MAX_NUM_THREADS + threadId.x;
@@ -141,32 +194,27 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
 
-    int W_unroll = C * K * K;
-    int H_unroll = H_out * W_out;
-    float* X_unrolled = (float*) malloc(W_unroll * H_unroll * sizeof(float));
-    for (int b=0; b < B; b++) {
-        unroll(C, H, W, K, b, x, X_unrolled);
-//        gemm(H_unroll, M, W_unroll, X_unrolled, W, Y[n]);
-    }
-//    // Set the kernel dimensions
-//    int W_grid = ceil(W_out/(float)TILE_WIDTH);
-//    // if (W_out%TILE_WIDTH != 0)
-//    //     W_grid++;
-//    int H_grid = ceil(H_out/(float)TILE_WIDTH);
-//    // if (H_out%TILE_WIDTH != 0)
-//    //     H_grid++;
-//    int Z = H_grid*W_grid;
-//
-//    dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
-//    dim3 gridDim(B, M, Z);
-//    // dim3 gridDim(0);
-//    // dim3 blockDim(0);
-//
-//    // Call the kernel
-//    forward_kernel<<<gridDim, blockDim>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
-//    // printf("%d", W_out);
-//    // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
-//    MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
+//    int W_unroll = C * K * K;
+//    int H_unroll = H_out * W_out;
+//    float* X_unrolled = (float*) malloc(W_unroll * H_unroll * sizeof(float));
+//    for (int b=0; b < B; b++) {
+//        unroll(C, H, W, K, b, x, X_unrolled);
+////        gemm(H_unroll, M, W_unroll, X_unrolled, W, Y[n]);
+//    }
+    // Set the kernel dimensions
+    int W_grid = ceil(W_out/(float)TILE_WIDTH);
+    int H_grid = ceil(H_out/(float)TILE_WIDTH);
+    int Z = H_grid*W_grid;
+
+    dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
+    dim3 gridDim(B, M, Z);
+    // share mem size
+    size_t shmem_size = sizeof(float) * ( (TILE_WIDTH + K-1)*(TILE_WIDTH + K-1) + K*K );
+    // Call the kernel
+    forward_kernel_shared<<<gridDim, blockDim, shmem_size>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
+    // printf("%d", W_out);
+    // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
+    MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
 
 }
 
